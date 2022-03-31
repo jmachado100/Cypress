f6f90ad338ddabe12102ddd735a62f9c2527f3ec
Diogo Pinto <diogo.martinho.pinto@hotmail.com>
2022-03-31 18:09:26 +0100

    history fix

--- cypress/integration/history/history.js
+++ cypress/integration/history/history.js
@@ -1,19 +1,20 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
-const url = 'http://localhost:8080/'
+const url = 'http://localhost:8081/'
+
     Given('I add or delete a todo element', () => {
         cy.visit(url)
-
         //add new element
         cy.get('.task-new__input').type('new element')
         cy.get('.task-new__field > .button').click()
 
-        localStorage.setItem('history',JSON.stringify([{id:1,name:"new element"}]))
 
-       
+
+
     })
 
 
 When(/^I click on the history button$/, function () {
+    localStorage.setItem('history',localStorage.getItem('tasks'))
     let button = '<input id="id1" value="History" type="button">';
     cy.get('.dashboard-info').then(function($button){
         $button.append(button);
@@ -21,22 +22,20 @@ When(/^I click on the history button$/, function () {
     cy.get('#id1').click()
 
     let val= JSON.parse(localStorage.getItem('history'))
-    let text = '<input id="id2" value="new element" name="text">';
+    console.log(val)
+    let text = ""
+
+    for(let i=0; i< val.length; i++) {
+        text += `<label id="element${i}">${val[i].name} </label><br>`;
+    }
     cy.get('.dashboard-content').then(function($input){
         $input.append(text);
     })
-    cy.get('#id2').then(function($input){
-        $input.innerText='new task'
-    })
-    
+
+
 });
 
 Then(/^I see the order of the actions taken$/, function () {
-/*
-    let input = '<input id="history_list">';
-    cy.get('.dashboard-content').then(function($input){
-        $input.append(input);
-    }).last().should('have.text','new element')
-    */
-    cy.get('input[name="text"]').contains('new element')
+
+    cy.get('#element3').contains('new element')
 });
